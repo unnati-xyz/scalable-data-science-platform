d729a0205dc7f0e76bee2ec808bad71ecf4520f5
nischal <nischal.hp@gmail.com>
2016-07-30 21:25:19 +0530

    Categorical codes method is fixed

--- poget/utils/ml.py
+++ poget/utils/ml.py
@@ -2,31 +2,39 @@ from delorean import parse
 import traceback
 
 from poget import LOGGER
-from poget.analytics.ml.terminal_traffic import TerminalTraffic
 
+'''
+
+This method is used to get hour
+'''
 def get_hour(value):
 
     parsed_date = parse(value).datetime.hour
     return parsed_date
+
 
 def get_day(value):
 
     parsed_date = parse(value).datetime.isoweekday()
     return parsed_date
 
-def get_categorical_codes(column):
+
+def get_categorical_codes(data_df, categorical_columns):
     try:
-        column = column.str.strip()
-        column = column.str.lower()
-        column = column.astype('category')
-        return column.apply(lambda x: x.cat.codes)
+        for column in categorical_columns:
+            print(column)
+            data_df[column] = data_df[column].str.strip()
+            data_df[column] = data_df[column].str.lower()
+            data_df[column] = data_df[column].astype('category')
+
+        modified_string = '_code'
+        categorical_columns_modified = [ column + modified_string for column in categorical_columns]
+
+        data_df[categorical_columns_modified] = data_df[categorical_columns].apply(lambda x: x.cat.codes)
+
+        return data_df
+
     except Exception as e:
         LOGGER.error(traceback.format_exc())
         raise e
-
-if __name__ == '__main__':
-    terminal_traffic = TerminalTraffic()
-    data = terminal_traffic.get_data()
-    data['Start Station Codes'] = data['Start Station'].apply(get_categorical_codes)
-    print(data.head())
 
